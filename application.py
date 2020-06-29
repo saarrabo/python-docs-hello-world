a06b13cd2434eb1872a5ec6a2234472c527ecfce
saarrabo <67553545+saarrabo@users.noreply.github.com>
2020-06-28 20:39:08 -0700

    Update application.py

--- application.py
+++ application.py
@@ -1,8 +1,8 @@
 #from pyimagesearch.transform import four_point_transform
-from skimage.filters import threshold_local
-import numpy as np
-import cv2
-import imutils
+#from skimage.filters import threshold_local
+#import numpy as np
+#import cv2
+#import imutils
 from flask import Flask, request, Response, send_file
 import jsonpickle
 app = Flask(__name__)
