3488ef5d57be5e9fab02412a4ffa6c23ee5ae20d
saarrabo <67553545+saarrabo@users.noreply.github.com>
2020-06-28 20:13:22 -0700

    Update application.py

--- application.py
+++ application.py
@@ -9,17 +9,7 @@ app = Flask(__name__)
 
 @app.route('/api/detectcard', methods=['POST'])
 def detectcard():
-    # r = request
-    # convert string of image data to uint8
-    # nparr = np.fromstring(r.data, np.uint8)
-    # decode image
-    # img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
-    # do some fancy processing here....
-
-    # build a response dict to send back to client
-    response = {'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0])
-                }
+    response = {'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0])}
     # encode response using jsonpickle
     response_pickled = jsonpickle.encode(response)
 
