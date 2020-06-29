f4de85885a86c68b0eeacad1c3b0bcaa04d6301d
saarrabo <67553545+saarrabo@users.noreply.github.com>
2020-06-28 17:48:56 -0700

    Update application.py

--- application.py
+++ application.py
@@ -8,11 +8,6 @@ from flask import Flask, request, Response, send_file
 import jsonpickle
 app = Flask(__name__)
 
-@app.route('/api/detectcard', methods=['POST'])
+@app.route('/api/detectcard')
 def detectcard():
-        # build a response dict to send back to client
-    response = {'message': 'image received.'}
-    # encode response using jsonpickle
-    response_pickled = jsonpickle.encode(response)
-
-    return Response(response=response_pickled, status=200, mimetype="application/json")    
+    return "Hello World"   
