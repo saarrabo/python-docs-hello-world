e22d576199389e0662167d228547ab11491ab665
saarrabo <67553545+saarrabo@users.noreply.github.com>
2020-06-28 19:02:06 -0700

    Update application.py

--- application.py
+++ application.py
@@ -1,11 +1,11 @@
-#from pyimagesearch.transform import four_point_transform
-#from skimage.filters import threshold_local
-#import numpy as np
-#import argparse
-#import cv2
-#import imutils
+from pyimagesearch.transform import four_point_transform
+from skimage.filters import threshold_local
+import numpy as np
+import argparse
+import cv2
+import imutils
 from flask import Flask, request, Response, send_file
-#import jsonpickle
+import jsonpickle
 app = Flask(__name__)
 
 @app.route('/api/detectcard', methods=['POST'])
