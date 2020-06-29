fea1feefee9321fb6781e3c20439d31b6706a83c
saarrabo <67553545+saarrabo@users.noreply.github.com>
2020-06-28 21:24:10 -0700

    Update application.py

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 #from pyimagesearch.transform import four_point_transform
 #from skimage.filters import threshold_local
-#import numpy as np
-#import cv2
+import numpy as np
+import cv2
 #import imutils
 from flask import Flask, request, Response, send_file
 import jsonpickle
@@ -10,4 +10,7 @@ app = Flask(__name__)
 @app.route('/api/detectcard', methods=['POST'])
 def detectcard():
     r = request
+    nparr = np.fromstring(r.data, np.uint8)
+    # decode image
+    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     return r.data
