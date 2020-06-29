5f593073bb21835bcabffd4cf4a2773909512ac1
saarrabo <67553545+saarrabo@users.noreply.github.com>
2020-06-28 19:14:59 -0700

    Update application.py

--- application.py
+++ application.py
@@ -1,7 +1,6 @@
 from pyimagesearch.transform import four_point_transform
 from skimage.filters import threshold_local
 import numpy as np
-import argparse
 import cv2
 import imutils
 from flask import Flask, request, Response, send_file
